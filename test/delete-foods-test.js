a0193894e4da8a46686ad34cb3c53aae1df1a088
Jason Conrad <jason.d.conrad.89@gmail.com>
2017-03-16 10:35:31 -0600

    [WIP] working on adding the totals to the total table in the diary
    Pushing up to a branch so I can Merge in changes to Master on my branch

---
+++ test/delete-foods-test.js
@@ -0,0 +1,14 @@
+
+before(function{
+  localSotrage.setItem()
+})
+
+
+
+context('Deleting foods', () => {
+    describe('when I click the red delete icon', () => {
+      it('the row is removed from the table', () => {
+
+      });
+    });
+  });
